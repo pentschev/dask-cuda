0e30bea6670ceb2d8bc56bca6f59d34b5fd2e092
Peter Andreas Entschev <peter@entschev.com>
2023-12-11 02:01:26 -0800

    Add test util to ensure subprocesses terminate

--- dask_cuda/utils_test.py
+++ dask_cuda/utils_test.py
@@ -1,4 +1,6 @@
-from typing import Literal
+import time
+from multiprocessing.process import BaseProcess
+from typing import Literal, Type, Union
 
 import distributed
 from distributed import Nanny, Worker
@@ -43,3 +45,43 @@ class IncreasedCloseTimeoutNanny(Nanny):
         self, timeout: float = 30.0, reason: str = "nanny-close"
     ) -> Literal["OK"]:
         return await super().close(timeout=timeout, reason=reason)
+
+
+def terminate_process(
+    process: Type[BaseProcess], kill_wait: Union[float, int] = 3.0
+) -> None:
+    """
+    Ensure a spawned process is terminated.
+
+    Ensure a spawned process is really terminated to prevent the parent process
+    (such as pytest) from freezing upon exit.
+
+    Parameters
+    ----------
+    process:
+        The process to be terminated.
+    kill_wait: float or integer
+        Maximum time to wait for the kill signal to terminate the process.
+
+    Raises
+    ------
+    RuntimeError
+        If the process terminated with a non-zero exit code.
+    ValueError
+        If the process was still alive after ``kill_wait`` seconds.
+    """
+    # Ensure process doesn't remain alive and hangs pytest
+    if process.is_alive():
+        process.kill()
+
+    start_time = time.monotonic()
+    while time.monotonic() - start_time < kill_wait:
+        if not process.is_alive():
+            break
+
+    if process.is_alive():
+        process.close()
+    elif process.exitcode != 0:
+        raise RuntimeError(
+            f"Process did not exit cleanly (exit code: {process.exitcode})"
+        )
